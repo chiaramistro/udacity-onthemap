9fa92f1d42599b2753cb91ef20b50d79d1960e43
Chiara Mistrorigo <chiaramistro@gmail.com>
2024-01-30 17:55:41 +0100

    Init app

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Chiara Mistrorigo on 30/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
