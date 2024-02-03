477d93b2b0b5e52bfcd6c83ec551be72b32e0772
Chiara Mistrorigo <chiaramistro@gmail.com>
2024-02-03 12:14:03 +0100

    Improve ui for login view controller

--- OnTheMap/Controller/LoginViewController.swift
+++ OnTheMap/Controller/LoginViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +25,31 @@ class LoginViewController: UIViewController {
     
     @IBAction func onLogin(_ sender: Any) {
         print("onLogin()")
-        self.performSegue(withIdentifier: "showHome", sender: nil)
+        
+        if (emailField.text?.isEmpty ?? true || passwordField?.text?.isEmpty ?? true) {
+           return
+        }
+        
+        setLoadingState(true)
+        UdacityClient.createSession(username: emailField.text!, password: passwordField.text!) { success, error in
+            print("createSession() success \(success) \(UdacityClient.Auth.sessionId)")
+            DispatchQueue.main.async {
+                self.setLoadingState(false)
+                self.performSegue(withIdentifier: "showHome", sender: nil)
+            }
+        }
+    }
+    
+    func setLoadingState(_ isLoading: Bool) {
+        if (isLoading) {
+            activityIndicator.startAnimating()
+        } else {
+            activityIndicator.stopAnimating()
+        }
+        emailField.isEnabled = !isLoading
+        passwordField.isEnabled = !isLoading
+        loginButton.isEnabled = !isLoading
+        signUpButton.isEnabled = !isLoading
     }
     
 
