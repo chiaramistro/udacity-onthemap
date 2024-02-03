958e6a243d6033be0dfa78530fe364027e35290b
Chiara Mistrorigo <chiaramistro@gmail.com>
2024-02-03 10:13:21 +0100

    Setup main screens with navigation

---
+++ OnTheMap/MapTabViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MapTabViewController.swift
+//  OnTheMap
+//
+//  Created by Chiara Mistrorigo on 31/01/24.
+//
+
+import Foundation
---
+++ OnTheMap/TableTabViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TableTabViewController.swift
+//  OnTheMap
+//
+//  Created by Chiara Mistrorigo on 31/01/24.
+//
+
+import UIKit
+
+class TableTabViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        print("TableTabViewController viewDidLoad()")
+    }
+}
+
