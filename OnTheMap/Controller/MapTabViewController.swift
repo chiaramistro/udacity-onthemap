634ee1eb1994017a44d1aea16d9456cf1d183a7e
Chiara Mistrorigo <chiaramistro@gmail.com>
2024-02-03 10:30:28 +0100

    New folder structure

--- OnTheMap/Controller/MapTabViewController.swift
+++ OnTheMap/Controller/MapTabViewController.swift
@@ -6,11 +6,16 @@
 //
 
 import UIKit
+import MapKit
 
-class MapTabViewController: UIViewController {
+class MapTabViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         print("MapTabViewController viewDidLoad()")
+
     }
+    
 }
 
