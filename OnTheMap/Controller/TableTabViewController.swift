67c5d96e5af710a9d64cbc51abebfb3e8f756fae
Chiara Mistrorigo <chiaramistro@gmail.com>
2024-02-06 15:21:09 +0100

    Display student data in the table tab

--- OnTheMap/Controller/TableTabViewController.swift
+++ OnTheMap/Controller/TableTabViewController.swift
@@ -7,10 +7,36 @@
 
 import UIKit
 
-class TableTabViewController: UIViewController {
+class TableTabViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         print("TableTabViewController viewDidLoad()")
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return StudentsModel.sharedInstance().students.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StudentCell")!
+        let student = StudentsModel.sharedInstance().students[(indexPath as NSIndexPath).row]
+
+        cell.textLabel?.text = student.firstName + " " + student.lastName
+
+//        FIXME: custom cell
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("tableView didSelectRowAt()")
+        // FIXME on click on cell
+    }
+
 }
 
