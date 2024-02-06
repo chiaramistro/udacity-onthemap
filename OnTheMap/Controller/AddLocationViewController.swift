519ac23da40093ccefd12a0ab00a6410e83587dc
Chiara Mistrorigo <chiaramistro@gmail.com>
2024-02-06 18:03:05 +0100

    Init add location view ui

--- OnTheMap/Controller/AddLocationViewController.swift
+++ OnTheMap/Controller/AddLocationViewController.swift
@@ -9,4 +9,16 @@ import UIKit
 
 class AddLocationViewController: UIViewController {
     
+    @IBOutlet weak var locationField: UITextField!
+    @IBOutlet weak var linkField: UITextField!
+    
+    override func viewDidLoad() {
+        print("AddLocationViewController viewDidLoad()")
+    }
+    
+    @IBAction func onFindLocation(_ sender: Any) {
+        print("onFindLocation()")
+        // TODO
+    }
+    
 }
