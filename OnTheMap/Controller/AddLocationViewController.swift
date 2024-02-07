f28425a1668eb502e58a93d6c5762d61b694f0b6
Chiara Mistrorigo <chiaramistro@gmail.com>
2024-02-07 15:32:04 +0100

    Add geocoding function and error/loading handling

--- OnTheMap/Controller/AddLocationViewController.swift
+++ OnTheMap/Controller/AddLocationViewController.swift
@@ -6,11 +6,16 @@
 //
 
 import UIKit
+import CoreLocation
 
 class AddLocationViewController: UIViewController {
     
     @IBOutlet weak var locationField: UITextField!
     @IBOutlet weak var linkField: UITextField!
+    
+    @IBOutlet weak var findLocationButton: UIButton!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         print("AddLocationViewController viewDidLoad()")
@@ -18,7 +23,43 @@ class AddLocationViewController: UIViewController {
     
     @IBAction func onFindLocation(_ sender: Any) {
         print("onFindLocation()")
-        // TODO
+        
+        setLoadingState(true)
+        
+        if (linkField?.text?.isEmpty ?? true) {
+            presentAlert(message: "Link field is empty")
+            return
+        }
+        
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(locationField?.text ?? "", completionHandler: {(placemarks, error) -> Void in
+            if error != nil {
+                self.presentAlert(message: "Some error occurred during geocoding")
+                return
+            }
+            if let placemark = placemarks?.first {
+                let coordinates:CLLocationCoordinate2D = placemark.location!.coordinate
+                self.setLoadingState(false)
+                // TODO show coordinates on map
+           }
+         })
     }
     
+    func setLoadingState(_ isLoading: Bool) {
+        if (isLoading) {
+            activityIndicator.startAnimating()
+        } else {
+            activityIndicator.stopAnimating()
+        }
+        locationField.isEnabled = !isLoading
+        linkField.isEnabled = !isLoading
+        findLocationButton.isEnabled = !isLoading
+    }
+    
+    func presentAlert(message: String) {
+        setLoadingState(false)
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
